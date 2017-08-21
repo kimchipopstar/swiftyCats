6c400fa44b60c5d0e0504560d0c7e5e65de0d518
J.K <K_940201@hotmail.com>
2017-08-20 17:54:28 -0700

    swiftyCats practice

--- swiftyCats/ViewController.swift
+++ swiftyCats/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+
+    let array:[UIImage] = [#imageLiteral(resourceName: "ed_sheeran"),#imageLiteral(resourceName: "taylor_swift"),#imageLiteral(resourceName: "justin_bieber")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return array.count
     }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCollectionCell", for: indexPath) as! photoCollectionCell
+        
+        cell.imageView.image = array[indexPath.row]
+        
+        return cell
+        
+    }
 
 }
 
---
+++ swiftyCats/photoCollectionCell.swift
@@ -0,0 +1,14 @@
+//
+//  photoCollectionCell.swift
+//  swiftyCats
+//
+//  Created by Jaewon Kim on 2017-08-20.
+//  Copyright © 2017 Jaewon Kim. All rights reserved.
+//
+
+import UIKit
+
+class photoCollectionCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
